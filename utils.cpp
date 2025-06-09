e87396965b11bf730e00755d534afbab8c13fefe
Admin <Admin@DESKTOP-LC407O9>
2025-06-09 18:02:32 +0200

    warunek brzegowy

--- utils.cpp
+++ utils.cpp
@@ -14,3 +14,10 @@ void init_condition(const double x[Nx], double U0[Nx]) {
         U0[i] = (x[i] < 0.0) ? 1.0 : 0.0;
     }
 }
+
+// Inicjalizacja warunku brzegowego
+void apply_boundary_conditions(double U[Nx]) {
+    U[0] = 1.0;
+    U[Nx - 1] = 0.0;
+}
+
--- utils.h
+++ utils.h
@@ -6,5 +6,7 @@
 // Funkcje inicjalizujące (zamiast zwracać wektory, modyfikują przekazane tablice)
 void init_space_grid(double x[Nx]);
 void init_condition(const double x[Nx], double U0[Nx]);
+void apply_boundary_conditions(double U[Nx]);
+
 
 #endif // UTILS_H

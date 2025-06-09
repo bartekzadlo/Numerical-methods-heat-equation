ec42654d3cb04fa50b094e4972811e6ca7aa1b63
Admin <Admin@DESKTOP-LC407O9>
2025-06-10 00:26:25 +0200

    Zmieniona ilość deklarowanych parametrów w params.h

--- params.h
+++ params.h
@@ -1,20 +1,8 @@
 #pragma once
-
 #include <cmath>
-
 namespace params {
     constexpr double D = 1.0;
     constexpr double t_max = 2.0;
-    constexpr double a = 6.0 * std::sqrt(D * t_max); // a >= 6 * sqrt(D * t_max)
 
-    constexpr double dx = 0.05;  // krok przestrzenny
-    constexpr double lambda_explicit = 0.4; // stabilny dla metody eksplicytnej
-    constexpr double dt_explicit = lambda_explicit * dx * dx / D;
-
-    constexpr double lambda_implicit = 1.0; // dla Crank-Nicolson
-    constexpr double dt_implicit = lambda_implicit * dx * dx / D;
-
-    constexpr int Nx = static_cast<int>(2 * a / dx) + 1;  // liczba punktów przestrzennych
-    constexpr int Nt_explicit = static_cast<int>(t_max / dt_explicit) + 1;
-    constexpr int Nt_implicit = static_cast<int>(t_max / dt_implicit) + 1;
+    inline double get_a() { return 6.0 * std::sqrt(D * t_max); }
 }
